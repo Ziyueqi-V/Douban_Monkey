45e00c7286be93d5d4a85cc986d29e537e297464
Ziyueqi-V <1208896581@qq.com>
2023-06-10 15:26:19 +0800

    release v0.0.2

--- Douban_Monkey.user.js
+++ Douban_Monkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Douban Beautify
 // @namespace    http://tampermonkey.net/
-// @version      0.0.3
+// @version      0.0.2
 // @description  try to take over the world!
 // @author       Sherlock-V
 // @match        https://douban.com
