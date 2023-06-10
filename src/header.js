0a462a4a734b2bb2e6bb57b9dc11a0860af5e9df
Ziyueqi-V <1208896581@qq.com>
2023-06-10 15:26:06 +0800

    release v0.0.3

--- src/header.js
+++ src/header.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Douban Beautify
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  try to take over the world!
 // @author       Sherlock-V
 // @match        https://douban.com
 // @match        https://*.douban.com/*
 // @icon         https://www.douban.com/favicon.ico
 // @grant        GM_addStyle
+// @license      MIT
 // ==/UserScript==
