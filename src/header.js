4f8bc7c1edfa35d057ed2b3585a2ee20a22d242a
Ziyueqi-V <1208896581@qq.com>
2023-06-11 09:43:48 +0800

    release v0.0.8

--- src/header.js
+++ src/header.js
@@ -2,12 +2,13 @@
 // @name         Douban_Monkey
 // @name:cn      豆瓣猴
 // @namespace    http://tampermonkey.net/
-// @version      0.0.7
+// @version      0.0.8
 // @description  douban beautify css injection script
 // @author       Sherlock-V
 // @match        https://douban.com
 // @match        https://*.douban.com/*
 // @icon         https://www.douban.com/favicon.ico
 // @grant        GM_addStyle
+// @supportURL   https://github.com/Ziyueqi-V/Douban_Monkey
 // @license      MIT
 // ==/UserScript==
