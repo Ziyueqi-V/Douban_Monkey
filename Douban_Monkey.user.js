8006bf9f47f4200e9c6590e152023cf2f66f2f11
Ziyueqi-V <1208896581@qq.com>
2023-07-19 00:57:15 +0800

    release v0.0.39

--- Douban_Monkey.user.js
+++ Douban_Monkey.user.js
@@ -2,7 +2,7 @@
 // @name         Douban_Monkey
 // @name:cn      豆瓣猴
 // @namespace    http://tampermonkey.net/
-// @version      0.0.38
+// @version      0.0.39
 // @description  douban beautify css injection script
 // @author       Sherlock-V
 // @match        https://douban.com
@@ -104,7 +104,7 @@ h1,
 .ep-info li span.all,
 .comments-app-wrapper.comments-app-wrapper,
 .blockquote-list.blockquote-list figure,
-html {
+.note.note p html {
   color: rgba(255, 255, 255, 0.65);
 }
 
